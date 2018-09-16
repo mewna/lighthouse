b6935775e78f8ebd27d3de067f94cbc57667ace3
amy null <null@amy.gg>
2018-09-16 16:11:10 -0400

    Service check TTLs

--- src/main/java/com/mewna/lighthouse/service/ConsulService.java
+++ src/main/java/com/mewna/lighthouse/service/ConsulService.java
@@ -92,7 +92,7 @@ public class ConsulService implements LighthouseService {
                         .setServiceId(id())
                         .setStatus(CheckStatus.PASSING)
                         .setInterval("1s")
-                        .setTtl("1s")
+                        .setTtl("60s")
                         .setDeregisterAfter("60s");
                 client.registerCheck(checkOptions, checkRes -> {
                     if(checkRes.succeeded()) {
