8556e05841d60a45bc4cb8470f797e093a2b8c13
amy null <null@amy.gg>
2018-09-16 21:07:06 -0400

    Remove consul service discovery

--- src/main/java/com/mewna/lighthouse/service/ConsulService.java
+++ src/main/java/com/mewna/lighthouse/service/ConsulService.java
@@ -1,10 +1,7 @@
 package com.mewna.lighthouse.service;
 
 import com.mewna.lighthouse.Lighthouse;
-import io.vertx.core.CompositeFuture;
 import io.vertx.core.Future;
-import io.vertx.core.http.HttpServer;
-import io.vertx.core.http.HttpServerOptions;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.consul.*;
 import lombok.Getter;
@@ -27,14 +24,10 @@ import java.util.stream.IntStream;
 @SuppressWarnings("unused")
 @RequiredArgsConstructor
 public class ConsulService implements LighthouseService {
-    public static final String CONSUL_SERVICE_NAME = "lighthouse-consul-node";
     private static final String CONSUL_SHARDING_LOCK = "lighthouse-consul-sharding-lock";
     private final Logger logger = LoggerFactory.getLogger(getClass());
     private final UUID id = UUID.randomUUID();
     private final Lighthouse lighthouse;
-    private final int healthcheckPort;
-    @SuppressWarnings("FieldCanBeLocal")
-    private HttpServer server;
     private ConsulClient client;
     @Getter
     private int shardId = -1;
@@ -50,71 +43,8 @@ public class ConsulService implements LighthouseService {
     public Future<LighthouseService> init(@Nonnull final String host) {
         final Future<LighthouseService> future = Future.future();
         
-        final Future<Void> serverFuture = Future.future();
-        final Future<Void> serviceFuture = Future.future();
-        final Future<Void> checkFuture = Future.future();
-        
         client = ConsulClient.create(lighthouse.vertx(), new ConsulClientOptions().setHost(host));
-        
-        server = lighthouse.vertx().createHttpServer(new HttpServerOptions().setPort(healthcheckPort))
-                .requestHandler(req -> {
-                    if(req.path().endsWith(id())) {
-                        logger.info("[Service] [{}] Accepted healthcheck for {}", id(), req.path());
-                        req.response().setStatusCode(200).end("OK");
-                    } else {
-                        logger.warn("[Service] [{}] Denied healthcheck for {}", id(), req.path());
-                        req.response().setStatusCode(500).end("NOTOK");
-                    }
-                }).listen(res -> {
-                    if(res.succeeded()) {
-                        logger.info("Healthcheck server listening on port {}", healthcheckPort);
-                        serverFuture.complete(null);
-                    } else {
-                        serverFuture.fail(res.cause());
-                    }
-                });
-        
-        final ServiceOptions serviceOptions = new ServiceOptions()
-                .setName(CONSUL_SERVICE_NAME)
-                .setTags(Collections.singletonList(id() + "  (Pod " + System.getenv("POD_NAME" + " )")))
-                .setId(id())
-                .setAddress(LighthouseService.getIp());
-        client.registerService(serviceOptions, res -> {
-            if(res.succeeded()) {
-                // Register checks
-                final CheckOptions checkOptions = new CheckOptions()
-                        .setId(id())
-                        .setName(id() + "  (Pod " + System.getenv("POD_NAME") + " )")
-                        .setHttp("http://" + LighthouseService.getIp() + ':' + healthcheckPort + "/lighthouse/check/" + id())
-                        .setServiceId(id())
-                        .setStatus(CheckStatus.PASSING)
-                        .setInterval("1s")
-                        .setDeregisterAfter("60s");
-                client.registerCheck(checkOptions, checkRes -> {
-                    if(checkRes.succeeded()) {
-                        logger.info("Healthchecks registered for {} id {}", CONSUL_SERVICE_NAME, id());
-                        checkFuture.complete(null);
-                    } else {
-                        logger.error("Couldn't register service healthchecks in consul!", checkRes.cause());
-                        checkFuture.fail(checkRes.cause());
-                    }
-                });
-                
-                logger.info("Successfully registered {} id {}", CONSUL_SERVICE_NAME, id());
-                serviceFuture.complete(null);
-            } else {
-                logger.error("Couldn't register service in consul!", res.cause());
-                serviceFuture.fail(res.cause());
-            }
-        });
-        
-        CompositeFuture.all(serverFuture, serviceFuture, checkFuture).setHandler(res -> {
-            if(res.succeeded()) {
-                future.complete(this);
-            } else {
-                future.fail(res.cause());
-            }
-        });
+        future.complete(this);
         
         return future;
     }
@@ -135,7 +65,6 @@ public class ConsulService implements LighthouseService {
                 .setName("lighthouse-sharding-lock")
                 .setTtl(15_000L)
                 .setLockDelay(10L)
-                .setChecks(Arrays.asList("serfHealth", id()))
                 .setBehavior(SessionBehavior.DELETE);
         client.createSessionWithOptions(sessionOpts, sessionRes -> {
             if(sessionRes.succeeded()) {
